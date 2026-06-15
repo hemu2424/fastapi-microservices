bf4ad791e9785139a0f20a332c229c8ca11b8e94
Himanshu Poriya <hporiya06@gmail.com>
2026-06-15 19:56:20 +0530

    fix docker login

--- api/app.py
+++ api/app.py
@@ -5,12 +5,12 @@ import os
 
 app = FastAPI()
 
-# MongoDB connection
+# MongoDB
 mongo_url = os.getenv("MONGO_URL", "mongodb://mongo:27017")
 client = MongoClient(mongo_url)
 db = client["mydb"]
 
-# Redis connection
+# Redis (safe connection)
 redis_host = os.getenv("REDIS_HOST", "redis")
 cache = redis.Redis(host=redis_host, port=6379, decode_responses=True)
 
@@ -22,17 +22,26 @@ def home():
 
 @app.get("/user")
 def get_user():
-    
-    # 1. check cache first
+
+    # Step 1: check cache
     cached_data = cache.get("user")
 
     if cached_data:
-        return {"source": "redis cache", "data": cached_data}
+        return {
+            "source": "redis cache",
+            "data": cached_data
+        }
 
-    # 2. if not in cache, fetch from DB
+    # Step 2: fetch from MongoDB
     user = db.users.find_one({}, {"_id": 0})
 
-    # 3. store in cache
+    if not user:
+        user = {"message": "No user found"}
+
+    # Step 3: store in cache
     cache.set("user", str(user), ex=30)
 
-    return {"source": "mongodb", "data": str(user)}
+    return {
+        "source": "mongodb",
+        "data": str(user)
+    }
